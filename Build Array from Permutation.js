e65d42751477f868e3c637bc2b4d33a643cf1ce0
Muhammed Abiola <hi@mabiola.net>
2022-08-16 13:55:59 +0100

    Concatenation and permutation array problems

---
+++ Build Array from Permutation.js
@@ -0,0 +1,9 @@
+var buildArray = function (nums) {
+  let ans = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = nums[nums[i]];
+  }
+
+  return ans;
+};
---
+++ Concatenation of Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  let ans = [];
+  let n = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = nums[i];
+    ans[i + n] = nums[i];
+  }
+
+  return ans;
+};
